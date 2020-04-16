e6f2074ed94016180ea2d3a9f1b8d0277be1bfbd
Otavio Camargo <otavioacamargo@outlook.com>
2020-04-16 01:29:22 -0300

    Add tests to vulnerable_zone model

--- spec/models/vulnerable_zone_spec.rb
+++ spec/models/vulnerable_zone_spec.rb
@@ -10,5 +10,33 @@ RSpec.describe VulnerableZone, type: :model do
       expect(vulnerable_zone.valid?).to be false
       expect(vulnerable_zone.errors[:cep]).to include("can't be blank")
     end
+
+    it 'is invalid without logradouro' do
+      vulnerable_zone.logradouro = nil
+
+      expect(vulnerable_zone.valid?).to be false
+      expect(vulnerable_zone.errors[:logradouro]).to include("can't be blank")
+    end
+
+    it 'is invalid without bairro' do
+      vulnerable_zone.bairro = nil
+
+      expect(vulnerable_zone.valid?).to be false
+      expect(vulnerable_zone.errors[:bairro]).to include("can't be blank")
+    end
+
+    it 'is invalid without localidade' do
+      vulnerable_zone.localidade = nil
+
+      expect(vulnerable_zone.valid?).to be false
+      expect(vulnerable_zone.errors[:localidade]).to include("can't be blank")
+    end
+
+    it 'is invalid without uf' do
+      vulnerable_zone.uf = nil
+
+      expect(vulnerable_zone.valid?).to be false
+      expect(vulnerable_zone.errors[:uf]).to include("can't be blank")
+    end
   end
 end
