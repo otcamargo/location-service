cf923993df0b7a49bc295cf07632f613ce213db5
Otávio Camargo <otavioacamargo@outlook.com>
2020-04-22 23:18:19 -0300

    Enable vulnerable_zones creation using a POST request

--- app/controllers/vulnerable_zones_controller.rb
+++ app/controllers/vulnerable_zones_controller.rb
@@ -2,5 +2,23 @@ class VulnerableZonesController < ApplicationController
   def index
     vulnerable_zones = VulnerableZone.all
     render json: vulnerable_zones
-  end 
+  end
+
+  def create
+    zip_code = params[:cep]
+    uri = "https://viacep.com.br/ws/#{zip_code}/json/"
+    response = HTTParty.get(uri)
+
+    vulnerable_zone_params = {
+      cep: response.parsed_response["cep"],
+      logradouro: response.parsed_response["logradouro"],
+      bairro: response.parsed_response["bairro"],
+      localidade: response.parsed_response["localidade"],
+      uf: response.parsed_response["uf"]
+    }
+
+    VulnerableZone.create!(vulnerable_zone_params)
+
+    render json: response
+  end
 end
