83356af434fe9152536463cfddcc62417c499f9e
Otávio Camargo <otavioacamargo@outlook.com>
2020-04-26 17:09:13 -0300

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :vulnerable_zones
-  delete 'vulnerable_zones/:cep', to: 'vulnerable_zones#destroy'
+  delete 'vulnerable_zones/', to: 'vulnerable_zones#destroy'
 end
