15905b75d27ea0c2fe3fde15d6924e9d644b8aa0
Otávio Camargo <otavioacamargo@outlook.com>
2020-04-28 01:49:39 -0300

    Add tests for Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  validates :nome, :email, :cep, :logradouro, :bairro, :localidade, :uf, presence: true
+  validates :email, uniqueness: true
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:person) { people(:person1) }
+
+  describe 'Validations' do
+
+    it 'is invalid without nome' do
+      person.nome = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:nome]).to include("can't be blank")
+    end
+
+    it 'is invalid without email' do
+      person.email = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:email]).to include("can't be blank")
+    end
+
+    it 'is invalid without cep' do
+      person.cep = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:cep]).to include("can't be blank")
+    end
+
+    it 'is invalid without logradouro' do
+      person.logradouro = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:logradouro]).to include("can't be blank")
+    end
+
+    it 'is invalid without bairro' do
+      person.bairro = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:bairro]).to include("can't be blank")
+    end
+
+    it 'is invalid without localidade' do
+      person.localidade = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:localidade]).to include("can't be blank")
+    end
+
+    it 'is invalid without uf' do
+      person.uf = nil
+
+      expect(person.valid?).to be false
+      expect(person.errors[:uf]).to include("can't be blank")
+    end
+  end
 end
